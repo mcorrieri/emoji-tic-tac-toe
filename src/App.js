24e403c88f7804fdab43d54f228856e4e3b963bc
Michael <Mlcorrieri@gmail.com>
2021-07-20 15:32:44 -0400

    basic board set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React from "react";
+import Game from "./Components/Game";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App-title">
       <h1>Emoji Tic Tac Toe!</h1>
+      <Game />
     </div>
   );
 }
---
+++ src/Components/Square.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function Square({ value, onClick }) {
+  return (
+    <button className="square" onClick={null}>
+      {value}
+    </button>
+  );
+}
+
+export default Square;
