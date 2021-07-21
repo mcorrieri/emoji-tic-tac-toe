89dbe7b465e4ab6ddf5148aa33cb9e5238f967a5
Michael <Mlcorrieri@gmail.com>
2021-07-21 17:50:33 -0400

    winner function is not a winner

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,8 +1,10 @@
+import { func } from "assert-plus";
 import React, { useState } from "react";
 
 function Game() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [nextTurn, setNextTurn] = useState(true);
+  const winner = whoWins(squares);
 
   function Square({ value, onClick }) {
     return (
@@ -25,6 +27,52 @@ function Game() {
       />
     );
   }
+
+  function whoWins(squares) {
+    const possibleWins = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < possibleWins.length; i++) {
+      const [a, b, c] = possibleWins[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+      {
+        return null;
+      }
+    }
+  }
+
+  function isBoardFull(squares) {
+    for (let i = 0; i < squares.length; i++)
+      if (squares[i] == null) {
+        return false;
+      } else {
+        return true;
+      }
+  }
+
+  function findWinner() {
+    if (winner) {
+      return "Winner" + winner;
+    } else if (isBoardFull(squares)) {
+      return "DRAW";
+    } else {
+      return "Next player: " + (nextTurn ? "X" : "O");
+    }
+  }
+
   return (
     <div className="container">
       <div className="game">
@@ -45,6 +93,7 @@ function Game() {
             {renderSquare(8)}
           </div>
         </div>
+        <div className="game-info">{findWinner()}</div>
       </div>
     </div>
   );
