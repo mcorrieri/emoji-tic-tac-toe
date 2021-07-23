df41551953f0be28ca08d9471d73ef1bda875619
Michael <Mlcorrieri@gmail.com>
2021-07-23 18:40:38 -0400

    emoji picker added

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,11 +1,18 @@
 import { func } from "assert-plus";
 import React, { useState } from "react";
+import { Dropdown } from "semantic-ui-react";
+import Picker from "emoji-picker-react";
 
 function Game() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [nextTurn, setNextTurn] = useState(true);
   const winner = whoWins(squares);
   const nextSymbol = nextTurn ? "X" : "O";
+  const [chosenEmoji, setChosenEmoji] = useState(null);
+
+  const onEmojiClick = (event, emojiObj) => {
+    setChosenEmoji(emojiObj);
+  };
 
   function Square({ value, onClick }) {
     return (
@@ -93,28 +100,50 @@ function Game() {
     }
   }
 
+  // const DropdownExampleSelection = () => (
+  //   <Dropdown
+  //     placeholder="Select Friend"
+  //     fluid
+  //     selection
+  //     options={emojiOptions}
+  //   />
+  // );
+
   return (
-    <div className="container">
-      <div className="game">
-        <div className="game-board">
-          <div className="board-row">
-            {renderSquare(0)}
-            {renderSquare(1)}
-            {renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {renderSquare(3)}
-            {renderSquare(4)}
-            {renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {renderSquare(6)}
-            {renderSquare(7)}
-            {renderSquare(8)}
-          </div>
+    <div>
+      <div className="player1-dropdown">
+        <div>
+          {chosenEmoji ? (
+            <span>You chose: {chosenEmoji.emoji}</span>
+          ) : (
+            <span>No emoji Chosen</span>
+          )}
+          <Picker onEmojiClick={onEmojiClick} />
         </div>
-        <div className="game-info">{findWinner()}</div>
-        <div className="restart-button">{renderRestartButton()}</div>
+      </div>
+      <div className="player2-dropdown"></div>
+      <div className="container">
+        <div className="game">
+          <div className="game-board">
+            <div className="board-row">
+              {renderSquare(0)}
+              {renderSquare(1)}
+              {renderSquare(2)}
+            </div>
+            <div className="board-row">
+              {renderSquare(3)}
+              {renderSquare(4)}
+              {renderSquare(5)}
+            </div>
+            <div className="board-row">
+              {renderSquare(6)}
+              {renderSquare(7)}
+              {renderSquare(8)}
+            </div>
+          </div>
+          <div className="game-info">{findWinner()}</div>
+          <div className="restart-button">{renderRestartButton()}</div>
+        </div>
       </div>
     </div>
   );
